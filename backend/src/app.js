083655734d63dcf513e298ffa8cf093cd142cc5d
subham-kr-singh <subhamkumarsingh077@gmail.com>
2026-07-11 00:24:18 +0530

    fix: standardize middleware formatting and add new user controller, model, and auth route files

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,13 +3,13 @@ import express from "express";
 const app = express();
 
 // middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
-app.use(express.json())
-app.use(cookieParser())
-app.use(morgan("dev"))
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
 
 // API Routes
-
+app.use("api/auth", authRoutes);
 
 // Default Error handler
 app.use((err, req, res, next) => {
@@ -17,7 +17,7 @@ app.use((err, req, res, next) => {
     const ErrStatusCode = err.statusCode || 500;
 
     res.status(ErrStatusCode).json({ message: ErrMessage });
-})
+});
 
 // Default API
 app.get("/", AuthProtect, (req, res) => {
@@ -27,7 +27,7 @@ app.get("/", AuthProtect, (req, res) => {
 app.use((req, res) => {
     res.status(404).json({
         message: "API Not Found"
-    })
-})
+    });
+});
 
 export default app;
